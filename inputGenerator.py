ac9708e54770769b3531569fe74ad625d2cbe980
Jorge Valdez <valdez.a.jorge@gmail.com>
2015-02-17 22:02:57 -0600

    Added input generator

---
+++ inputGenerator.py
@@ -0,0 +1,33 @@
+import random
+
+f = open('input.txt', 'w')
+
+letters = 'abcdefghijklmnopqrstuvwxyz'
+capLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+ssize = random.randint(0, 50)
+inputsize = random.randint(0, 1000)
+
+sentenceCount = 0
+iscap = False
+
+for i in range(inputsize):
+    word = ''
+
+    if sentenceCount is 10:
+        f.write('.\n')
+        iscap = True
+
+    for j in range(ssize):
+        if iscap:
+            word += random.choice(capLetters)
+            iscap = False
+            sentenceCount = 0
+
+        else:
+            word += random.choice(letters)
+
+
+    f.write(' ' + word)
+    ssize = random.randint(0, 50)
+    sentenceCount += 1
