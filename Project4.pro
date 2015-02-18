73fce3147dfc734b0a797b4174c70ebb891e2904
Jayce Miller <jaycem@smu.edu>
2015-02-17 21:09:07 -0600

    added time package

---
+++ Project4.pro
@@ -0,0 +1,14 @@
+TEMPLATE = app
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += main.cpp \
+    SortingCompetition.cpp
+
+OTHER_FILES += \
+    README.md
+
+HEADERS += \
+    SortingCompetition.h
+
--- main.cpp
+++ main.cpp
@@ -7,12 +7,36 @@
 //
 
 #include <iostream>
+#include <chrono>
+#include <ctime>
 
 using namespace std;
+using namespace chrono;
 
-int main(int argc, const char * argv[])
+long fibonacci(int n)
 {
-    // insert code here...
-    cout << "Hello, World!" << endl;
-    return 0;
+    if (n < 3) return 1;
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main()
+{
+    //declare 2 time points
+      time_point<chrono::system_clock> start, end;
+
+    //store the current time (now()) in start, execute
+    //Fibonacci function, then store current time in end
+    start = system_clock::now();
+    cout << "f(42) = " << fibonacci(9) << '\n';
+    end = system_clock::now();
+
+    //subtract end from beginning to get number of seconds elapsed
+      duration<double> elapsed_seconds = end-start;
+    time_t end_time =
+    system_clock::to_time_t(end);
+
+    //output the duration.
+    cout << "finished computation at " <<
+    ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s\n";
 }
