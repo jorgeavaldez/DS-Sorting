1094537e465fefdfd34633a1a5a6b9518c532a19
Jorge Valdez <valdez.a.jorge@gmail.com>
2015-02-20 13:05:16 -0600

    wat

--- quicksort.cpp
+++ quicksort.cpp
@@ -15,29 +15,34 @@ void QuickSort::sortData()
 
 void QuickSort::quickSort(char** &inputWords, int left, int right)
 {
-      int i = left, j = right;
-      char* temp;
-      int pivot = strlen(inputWords[(left + right) / 2]);
+    int i = left;
+    int j = right;
+    char* temp;
+    long pivot = strlen(inputWords[(left + right) / 2]);
 
-      while (i <= j) {
-            while (strlen(inputWords[i]) < pivot)
-                  i++;
-            while (strlen(inputWords[j]) > pivot)
-                  j--;
-            if (i <= j) {
-                  tmp = inputWords[i];
-                  inputWords[i] = inputWords[j];
-                  inputWords[j] = inputWords;
-                  i++;
-                  j--;
-            }
-      }
-      if (left < j)
-            quickSort(inputWords, left, j);
-      if (i < right)
-            quickSort(inputWords, i, right);
+    while (i <= j)
+    {
+        while (strlen(inputWords[i]) < pivot)
+            i++;
+           
+        while (strlen(inputWords[j]) > pivot)
+            j--;
+          
+        if (i <= j)
+        {
+                // Swap
+            temp = inputWords[i];
+            inputWords[i] = inputWords[j];
+            inputWords[j] = temp;
+                
+            i++;
+            j--;
+        }
+    }
+    
+    if (left < j)
+        quickSort(inputWords, left, j);
+    
+    if (i < right)
+        quickSort(inputWords, i, right);
 }
-      /* recursion */
-
-
-
