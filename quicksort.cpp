2dfe7e64f2b100e02251538ed82831269856cdc8
Jorge Valdez <valdez.a.jorge@gmail.com>
2015-02-20 13:07:52 -0600

    Fixed merge fufu ass lame

--- quicksort.cpp
+++ quicksort.cpp
@@ -15,7 +15,6 @@ void QuickSort::sortData()
 
 void QuickSort::quickSort(char** &inputWords, int left, int right)
 {
-<<<<<<< HEAD
     int i = left;
     int j = right;
     char* temp;
@@ -47,31 +46,3 @@ void QuickSort::quickSort(char** &inputWords, int left, int right)
     if (i < right)
         quickSort(inputWords, i, right);
 }
-=======
-      int i = left, j = right;
-      char* temp;
-      int pivot = strlen(inputWords[(left + right) / 2]);
-
-      while (i <= j) {
-            while (strlen(inputWords[i]) < pivot)
-                  i++;
-            while (strlen(inputWords[j]) > pivot)
-                  j--;
-            if (i <= j) {
-                  temp = inputWords[i];
-                  inputWords[i] = inputWords[j];
-                  inputWords[j] = temp;
-                  i++;
-                  j--;
-            }
-      }
-      if (left < j)
-            quickSort(inputWords, left, j);
-      if (i < right)
-            quickSort(inputWords, i, right);
-}
-      /* recursion */
-
-
-
->>>>>>> c94a282f4cd84f1b98916f0628126e4486c0a831
